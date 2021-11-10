b086e97ed502e24856dcaf2c12e14b3b8fca98cb
gomiseki <kimju1755@gmail.com>
2021-11-11 02:36:36 +0900

    [update] css reset, veiw 크기 조정

--- src/main.js
+++ src/main.js
@@ -9,14 +9,13 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    minWidth: 1200,
+    minHeight: 800,
   });
 
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
