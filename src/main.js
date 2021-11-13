1a57e62507437f082f5671287d745a43252f8474
gomiseki <kimju1755@gmail.com>
2021-11-13 17:55:58 +0900

    [update] 아이쿠손이 미끄러졌네

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-import express from 'express';
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
