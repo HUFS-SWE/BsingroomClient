99224e7f5da7fa148d11b5e702b260fe0f3d0d77
gomiseki <kimju1755@gmail.com>
2021-11-15 12:12:00 +0900

    [add]redux

--- src/app.jsx
+++ src/app.jsx
@@ -4,17 +4,22 @@ import{ HashRouter, Route, Routes} from 'react-router-dom';
 import Intro from './component/intro/intro';
 import Lobby from './component/lobby/lobby';
 import Room from './component/room/room';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer); 
 
 function render() {
   ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={store}>
+      <React.StrictMode>
         <HashRouter>
-        <Routes>
-        <Route path="/" element={<Intro/>} />
-        <Route path="/lobby"  element={<Lobby/>} />
-        <Route path="/room"  element={<Room/>} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Intro/>} />
+            <Route path="/lobby"  element={<Lobby/>} />
+            <Route path="/room"  element={<Room/>} />
+          </Routes>
         </HashRouter>
-    </React.StrictMode>, document.body);
+      </React.StrictMode>
+    </Provider>, document.body);
 }
 render();
